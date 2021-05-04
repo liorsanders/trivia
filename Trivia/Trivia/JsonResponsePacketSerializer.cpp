538b09b91617de3bd4961c3d1b9a497a78d07403
gal pahima <galpahima2508@gmail.com>
2021-05-04 21:35:09 +0300

    feat: added function that turn string to binary digits

--- Trivia/Trivia/JsonResponsePacketSerializer.cpp
+++ Trivia/Trivia/JsonResponsePacketSerializer.cpp
@@ -1,1 +1,48 @@
 #include "JsonResponsePacketSerializer.h"
+
+
+std::vector<unsigned char> 
+	JsonResponsePacketSerializer::serializeResponse(ErrorResponse& response)
+{
+	return serialize<std::string>(response.message, "message");
+}
+
+std::vector<unsigned char> 
+	JsonResponsePacketSerializer::serializeResponse(LoginResponse& response)
+{
+	return serialize<unsigned int>(response.status, "status");
+}
+
+std::vector<unsigned char> 
+	JsonResponsePacketSerializer::serializeResponse(SignupResponse& response)
+{
+	return serialize<unsigned int>(response.status, "status");
+}
+
+string JsonResponsePacketSerializer::stringToBinary(const string& data)
+{
+	int length = data.length();
+	string binaryData = "", bin = "";
+
+	for (auto& i : data)
+	{
+		binaryData += asciiToBytes(int(i)) + " ";
+	}
+
+	return binaryData;
+}
+
+string JsonResponsePacketSerializer::asciiToBytes(int letter)
+{
+	string binaryString = "";
+
+	while (letter > 0)
+	{
+		letter % 2 ? binaryString.push_back('1') : binaryString.push_back('0');
+		letter /= 2;
+	}
+
+	reverse(binaryString.begin(), binaryString.end());
+
+	return binaryString;
+}
