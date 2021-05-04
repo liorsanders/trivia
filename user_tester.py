424ac3bd029d2ab5e3dba96f0dc40abec5e4a0eb
lior sanders <sanderslior@gmail.com>
2021-05-04 21:13:41 +0300

    implemented tester

--- user_tester.py
+++ user_tester.py
@@ -1,7 +1,14 @@
 import socket 
+import pickle
 
 CONFIG_FILE_NAME = 'config.txt'
 MSG_WITH_SERVER = 'Hello'
+MAX_RECV = 1024
+USERNAME = 'user1'
+PASSWORD = '1234'
+
+LOGIN_CODE = '\x01'.encode()
+SIGN_UP_CODE = '\x02'.encode()
 
 # return (server_ip, server_port)
 def get_config_info(f_name):
@@ -10,19 +17,29 @@ def get_config_info(f_name):
         return lines[0][lines[0].index('=')+1:], int(lines[1][lines[1].index('=')+1:])
         
 
+def recieve_msg(sock):
+    ans = sock.recv(MAX_RECV)
+    print(f'recieved from server: {pickle.loads(ans)}') # desirializing and printing data from server
+
 def main():
     try:
         # Create a TCP/IP socket
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            # Connect the socket to the port where the server is listening
             sock.connect(get_config_info(CONFIG_FILE_NAME))
-
             print('connected to server..')
-            data = sock.recv(len(MSG_WITH_SERVER))
-            print(f'msg from mt server: {data.decode()}')
-            print(f'sending: {MSG_WITH_SERVER}')
-            sock.sendall(MSG_WITH_SERVER.encode())
-
+            login = {'username': USERNAME, 'password': PASSWORD}
+            sign_up = {'username': USERNAME, 'password': PASSWORD, 'mail': 'user1@gmail.com'}
+            print(f'sending: {sign_up} to server')
+            data = bytearray(pickle.dumps(sign_up)) # serialize dict before sending
+            data[0:0] = SIGN_UP_CODE # insert message code
+            sock.sendall(data)
+            recieve_msg(sock)
+            print(f'sending: {login} to server')
+            data = bytearray(pickle.dumps(login))
+            data[0:0] = LOGIN_CODE # insert message code
+            sock.sendall(data)
+            recieve_msg(sock)
+            
     except Exception as e:
         print(f'error: {e}')
 
