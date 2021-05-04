d91818721702108b1b390015e9f17218fe76cc6b
gal pahima <galpahima2508@gmail.com>
2021-05-04 21:11:56 +0300

    style: change header file so now only the tamplate function is there and all the class

--- Trivia/Trivia/JsonResponsePacketSerializer.h
+++ Trivia/Trivia/JsonResponsePacketSerializer.h
@@ -4,29 +4,38 @@
 #include "SignupResponse.h"
 #include "LoginResponse.h"
 #include "json.hpp"
+#include <iostream>
+#include <algorithm>
+#include <string>
 
 class JsonResponsePacketSerializer
 {
 public:
-	static std::vector<unsigned char> serializeResponse(ErrorResponse response)
-	{
+	static std::vector<unsigned char> serializeResponse(ErrorResponse response);
 
-	}
+	static std::vector<unsigned char> serializeResponse(LoginResponse response);
 
-	static std::vector<unsigned char> serializeResponse(LoginResponse response)
-	{
-
-	}
-
-	static std::vector<unsigned char> serializeResponse(SignupResponse response)
-	{
-
-	}
+	static std::vector<unsigned char> serializeResponse(SignupResponse response);
 
 private:
 	template <typename Response>
-	static std::vector<unsigned char> serializeResponse(SignupResponse response)
+
+	static std::vector<unsigned char> serialize(Response response)
 	{
+		nlohmann::json data;
+
+		data["status"] = response.status;
+
+		std::string jsonString = data.dump();
+
+		std::string binaryString = stringToBinary(jsonString);
+
+		return std::vector<unsigned char>(binaryString.begin(), binaryString.end());
 	}
+
+	static std::string stringToBinary(std::string& data);
+
+	static std::string asciiToBytes(int letter);
+
 };
 
