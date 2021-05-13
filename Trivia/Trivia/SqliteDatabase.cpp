07c3bf9cb184b6313acd52a80ffb946d06df2340
gal pahima <galpahima2508@gmail.com>
2021-05-13 23:10:10 +0300

    feat: added sqlite3 data base with constractor to the class

--- Trivia/Trivia/SqliteDatabase.cpp
+++ Trivia/Trivia/SqliteDatabase.cpp
@@ -1,1 +1,36 @@
 #include "SqliteDatabase.h"
+#include <iostream>
+
+SqliteDatabase::SqliteDatabase()
+{
+	DB = std::make_shared<sqlite3>();
+
+	createDB();
+}
+
+void SqliteDatabase::createDB()
+{
+	sqlite3* rawDB = DB.get();
+
+	int isBad = sqlite3_open("users.db", &rawDB);
+
+	if (isBad)
+	{
+		std::cerr << "Can't open database: " << sqlite3_errmsg(rawDB) << std::endl;
+	}
+	else 
+	{
+		std::cerr << "Opened database successfully!" << std::endl;
+	}
+
+	delete rawDB;
+}
+
+SqliteDatabase::~SqliteDatabase()
+{
+	sqlite3* rawDB = DB.get();
+
+	sqlite3_close(rawDB);
+
+	delete rawDB;
+}
--- Trivia/Trivia/SqliteDatabase.h
+++ Trivia/Trivia/SqliteDatabase.h
@@ -1,17 +1,19 @@
 #pragma once
 #include "IDatabase.h"
-#include <winsqlite/winsqlite3.h>
+#include "sqlite3.h"
+#include <memory>
 
 class SqliteDatabase : IDatabase
 {
 public:
 	SqliteDatabase();
+	void createDB();
 	~SqliteDatabase();
 	virtual bool doesUserExist(std::string username);
 	virtual bool doesPasswordMatch(std::string username, std::string password);
 	virtual void addNewUser(std::string username, std::string password, std::string mail);
 
 private:
-	sqlite3* db;
+	std::shared_ptr<sqlite3> DB;
 };
 
