7dba91a13e34c939f28a66fe0df8b95c31e16771
gal pahima <galpahima2508@gmail.com>
2021-05-13 23:14:03 +0300

    feat: added createTable function that create sql table with name password and mail

--- Trivia/Trivia/SqliteDatabase.cpp
+++ Trivia/Trivia/SqliteDatabase.cpp
@@ -3,34 +3,38 @@
 
 SqliteDatabase::SqliteDatabase()
 {
-	DB = std::make_shared<sqlite3>();
-
 	createDB();
+	createTable();
+}
+
+void SqliteDatabase::createTable()
+{
+	std::string sql = "CREATE TABLE users("
+		"ID	INTEGER,"
+		"Name TEXT NOT NULL,"
+		"Password TEXT NOT NULL,"
+		"Mail TEXT NOT NULL,"
+		"PRIMARY KEY(ID AUTOINCREMENT)"
+		");";
+
+	int isBad = sqlite3_exec(db, sql.c_str(), NULL, NULL, NULL);
 }
 
 void SqliteDatabase::createDB()
 {
-	sqlite3* rawDB = DB.get();
-
-	int isBad = sqlite3_open("users.db", &rawDB);
+	int isBad = sqlite3_open("users.db", &db);
 
 	if (isBad)
 	{
-		std::cerr << "Can't open database: " << sqlite3_errmsg(rawDB) << std::endl;
+		std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
 	}
 	else 
 	{
 		std::cerr << "Opened database successfully!" << std::endl;
 	}
-
-	delete rawDB;
 }
 
 SqliteDatabase::~SqliteDatabase()
 {
-	sqlite3* rawDB = DB.get();
-
-	sqlite3_close(rawDB);
-
-	delete rawDB;
+	sqlite3_close(db);
 }
--- Trivia/Trivia/SqliteDatabase.h
+++ Trivia/Trivia/SqliteDatabase.h
@@ -1,19 +1,20 @@
 #pragma once
 #include "IDatabase.h"
 #include "sqlite3.h"
-#include <memory>
 
 class SqliteDatabase : IDatabase
 {
 public:
 	SqliteDatabase();
-	void createDB();
 	~SqliteDatabase();
 	virtual bool doesUserExist(std::string username);
 	virtual bool doesPasswordMatch(std::string username, std::string password);
 	virtual void addNewUser(std::string username, std::string password, std::string mail);
 
 private:
-	std::shared_ptr<sqlite3> DB;
+	sqlite3* db;
+
+	void createTable();
+	void createDB();
 };
 
