770a0bfddf793b18ce52e78a7e6c3e4b292c6b7a
gal pahima <galpahima2508@gmail.com>
2021-05-13 16:35:29 +0300

    style: change get_server_details to be more readable

--- Python/user_tester.py
+++ Python/user_tester.py
@@ -1,46 +1,55 @@
-import socket 
-import pickle
+import socket
 import constants
 
-# return (server_ip, server_port)
-def get_config_info(f_name):
-    with open(f_name, 'r') as config_file:
-        lines = config_file.read().split('\n')
-        return lines[0][lines[0].index('=')+1:], int(lines[1][lines[1].index('=')+1:])
-        
 
-def recieve_msg(sock):
+# return (server_ip, server_port)
+def get_server_details(file_name):
+    config_file = open(file_name, "r")
+
+    server_port = config_file.readline()
+    server_port = server_port[server_port.find('='):]
+
+    server_ip = config_file.readline()
+    server_ip = server_ip[server_ip.find('='):]
+
+    return server_port, server_ip
+
+
+def receive_message(sock):
     ans = sock.recv(constants.MAX_RECV)
-    print(f'recieved from server: {pickle.loads(ans)}') # desirializing and printing data from server
+    # To Do: deserialize message
+
 
 def sign_up(sock):
     data = {'username': constants.USERNAME, 'password': constants.PASSWORD, 'mail': constants.USER_MAIL}
     print(f'sending: {data} to server')
-    data = bytearray(pickle.dumps(data)) # serialize dict before sending
-    data[0:0] = constants.SIGN_UP_CODE # insert message code
+    data = bytearray(pickle.dumps(data))  # serialize dict before sending
+    data[0:0] = constants.SIGN_UP_CODE  # insert message code
     sock.sendall(data)
-    
+
+
 def log_in(sock):
     data = {'username': constants.USERNAME, 'password': constants.PASSWORD}
     print(f'sending: {data} to server')
     data = bytearray(pickle.dumps(data))
-    data[0:0] = constants.LOGIN_CODE # insert message code
+    data[0:0] = constants.LOGIN_CODE  # insert message code
     sock.sendall(data)
-    
+
 
 def main():
     try:
         # Create a TCP/IP socket
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect(get_config_info(constants.CONFIG_FILE_NAME))
+            sock.connect(get_server_details(constants.CONFIG_FILE_NAME))
             print('connected to server..')
             sign_up(sock)
-            recieve_msg(sock)
+            receive_message(sock)
             log_in(sock)
-            recieve_msg(sock)            
-            
+            receive_message(sock)
+
     except Exception as e:
         print(f'error: {e}')
+
 
 if __name__ == '__main__':
     main()
