e9df23de9a1b1116ceff0853518b8e3185427532
gal pahima <galpahima2508@gmail.com>
2021-05-13 17:26:40 +0300

    fix: the client send the wrong message so I change sign up and login

--- Python/user_tester.py
+++ Python/user_tester.py
@@ -1,54 +1,80 @@
 import socket
 import constants
+import json
 
 
 # return (server_ip, server_port)
 def get_server_details(file_name):
     config_file = open(file_name, "r")
 
-    server_port = config_file.readline()
-    server_port = server_port[server_port.find('='):]
-
     server_ip = config_file.readline()
-    server_ip = server_ip[server_ip.find('='):]
+    server_ip = server_ip[server_ip.find('=') + 1:].replace("\n", "")
 
-    return server_port, server_ip
+    server_port = config_file.readline()
+    server_port = int(server_port[server_port.find('=') + 1:])
+
+    return server_ip, server_port
 
 
 def receive_message(sock):
-    ans = sock.recv(constants.MAX_RECV)
+    answer = sock.recv(constants.MAX_RECV)
     # To Do: deserialize message
 
 
+def length_to_four_bytes(data):
+    data_length = len(data)
+
+    return data_length.to_bytes(4, 'big')
+
+
 def sign_up(sock):
-    data = {'username': constants.USERNAME, 'password': constants.PASSWORD, 'mail': constants.USER_MAIL}
-    print(f'sending: {data} to server')
-    data = bytearray(pickle.dumps(data))  # serialize dict before sending
-    data[0:0] = constants.SIGN_UP_CODE  # insert message code
-    sock.sendall(data)
+    data = {
+        "Username": constants.USERNAME,
+        "Password": constants.PASSWORD,
+        "Mail": constants.USER_MAIL
+    }
+    json_data = json.dumps(data)
+    print("sending:", json_data, ", to server.")
+
+    full_message = constants.SIGN_UP_CODE + \
+                   length_to_four_bytes(json_data) + \
+                   json_data.encode()
+
+    sock.sendall(full_message)
 
 
 def log_in(sock):
-    data = {'username': constants.USERNAME, 'password': constants.PASSWORD}
-    print(f'sending: {data} to server')
-    data = bytearray(pickle.dumps(data))
-    data[0:0] = constants.LOGIN_CODE  # insert message code
-    sock.sendall(data)
+    data = {
+        "Username": constants.USERNAME,
+        "Password": constants.PASSWORD,
+    }
+
+    json_data = json.dumps(data)
+    print("sending:", json_data, ", to server.")
+
+    full_message = constants.LOGIN_CODE + \
+                   length_to_four_bytes(json_data) + \
+                   json_data.encode()
+
+    sock.sendall(full_message)
 
 
 def main():
     try:
         # Create a TCP/IP socket
+
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect(get_server_details(constants.CONFIG_FILE_NAME))
+
             print('connected to server..')
+
             sign_up(sock)
             receive_message(sock)
             log_in(sock)
             receive_message(sock)
 
     except Exception as e:
-        print(f'error: {e}')
+        print("error:", {e})
 
 
 if __name__ == '__main__':
