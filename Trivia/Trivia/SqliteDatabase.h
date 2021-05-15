36fc4442ec02701ecb64e82af5e6b0705f5c37ab
gal pahima <galpahima2508@gmail.com>
2021-05-15 15:33:32 +0300

    style: added const and override when needed

--- Trivia/Trivia/SqliteDatabase.h
+++ Trivia/Trivia/SqliteDatabase.h
@@ -2,27 +2,26 @@
 #include "IDatabase.h"
 #include "sqlite3.h"
 
-class SqliteDatabase /*: IDatabase*/
+class SqliteDatabase : IDatabase
 {
 public:
-	SqliteDatabase();
-	~SqliteDatabase();
+	SqliteDatabase(); 
+	~SqliteDatabase() override;
 
-	virtual bool doesUserExist(std::string username) const;
+	bool doesUserExist(std::string username) const override;
 
-	virtual bool doesPasswordMatch
-		(std::string username, std::string password) const;
+	bool doesPasswordMatch
+		(std::string username, std::string password) const override;
 
-	virtual void addNewUser
-		(std::string username, std::string password, std::string mail);
-
+	void addNewUser
+			(std::string username, std::string password, std::string mail) override;
 private:
 	sqlite3* db;
 
 	void createTable();
 	void createDB();
 	static int doeasExistsCallback
-	(void* isExists, int argc, char ** argv, char ** cols);
+		(void* isExists, int argc, char** argv, char** cols);
 
 };
 
