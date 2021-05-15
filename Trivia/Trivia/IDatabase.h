b7ec088800f9612756ffb81b2d7f31219f616c5b
gal pahima <galpahima2508@gmail.com>
2021-05-15 15:53:24 +0300

    fix: IDatabase dtor cannot be =0 because dtor cannot be left undefined so =default

--- Trivia/Trivia/IDatabase.h
+++ Trivia/Trivia/IDatabase.h
@@ -5,7 +5,7 @@
 class IDatabase
 {
 public:
-	virtual ~IDatabase() = 0;
+	virtual ~IDatabase() = default;
 	virtual bool doesUserExist(std::string username) const = 0;
 	virtual bool doesPasswordMatch(std::string username, std::string password) const = 0;
 	virtual void addNewUser(std::string username, std::string password, std::string mail) = 0;
