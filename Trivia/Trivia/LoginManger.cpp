a31cd3ac204d7c5c7c8505ed7f146498b3f652cc
gal pahima <galpahima2508@gmail.com>
2021-05-21 13:48:57 +0300

    build: added empty LoginManager class with function declarations

---
+++ Trivia/Trivia/LoginManger.cpp
@@ -0,0 +1,1 @@
+#include "LoginManger.h"
---
+++ Trivia/Trivia/LoginManger.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include "SqliteDatabase.h"
+#include "LoggedUser.h"
+
+class LoginManger
+{
+public:
+	void signup(std::string username, std::string password, std::string mail);
+	void login(std::string username, std::string password);
+	void logout(std::string password);
+private:
+	std::shared_ptr<IDatabase> m_database;
+	std::vector<LoggedUser> m_loggedUsers;
+};	
+
