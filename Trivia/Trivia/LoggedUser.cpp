471d49d07462f2c068ca10a3c84925a54bf5c132
gal pahima <galpahima2508@gmail.com>
2021-05-21 16:00:23 +0300

    feat: added ctors and dtor

--- Trivia/Trivia/LoggedUser.cpp
+++ Trivia/Trivia/LoggedUser.cpp
@@ -1,5 +1,10 @@
 #include "LoggedUser.h"
 
+LoggedUser::LoggedUser( const std::string& username) :
+    m_username(username)
+{ 
+}
+
 std::string LoggedUser::getUsername()
 {
     return m_username;
--- Trivia/Trivia/LoggedUser.h
+++ Trivia/Trivia/LoggedUser.h
@@ -5,8 +5,16 @@
 class LoggedUser
 {
 public: 
-	std::string getUsername();	
+	// ctors
+	LoggedUser() = default;
+	LoggedUser(const std::string& username);
 
+	// dtor
+	~LoggedUser() = default;
+
+	// getters
+	std::string getUsername();	
+	
 private:
 	std::string m_username;
 };
