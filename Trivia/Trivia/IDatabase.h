d5f29792bae17a66ef58bac6b408d8dd5889c2c0
gal pahima <galpahima2508@gmail.com>
2021-05-21 17:17:54 +0300

    fix: added database files from develop to make this branch work

--- Trivia/Trivia/IDatabase.h
+++ Trivia/Trivia/IDatabase.h
@@ -5,7 +5,8 @@
 class IDatabase
 {
 public:
-	virtual bool doesUserExist(std::string username) = 0;
-	virtual bool doesPasswordMatch(std::string username, std::string password) = 0;
+	virtual ~IDatabase() = default;
+	virtual bool doesUserExist(std::string username) const = 0;
+	virtual bool doesPasswordMatch(std::string username, std::string password) const = 0;
 	virtual void addNewUser(std::string username, std::string password, std::string mail) = 0;
 };
