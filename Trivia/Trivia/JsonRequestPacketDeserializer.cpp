808f451c5b34ee62020132acc1b8a9e0dde3b019
gal pahima <galpahima2508@gmail.com>
2021-05-23 18:34:56 +0300

    feat: add option to deserialize login request

---
+++ Trivia/Trivia/JsonRequestPacketDeserializer.cpp
@@ -0,0 +1,26 @@
+#include "JsonRequestPacketDeserializer.h"
+#include "json.hpp"
+#include <iostream>
+
+LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest
+    (std::vector<unsigned char> buffer)
+{
+    LoginRequest request = LoginRequest();
+
+    // json can accept string as a paramter so we convert the vector to string
+    nlohmann::json data = nlohmann::json::parse(
+        std::string(buffer.begin(), buffer.end()));
+
+    // in case some one try to trick us and there is no user name
+    try
+    {
+        request.username = data["username"];
+        request.username = data["password"];
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what();
+    }
+ 
+    return request;
+}
