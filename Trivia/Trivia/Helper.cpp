608e0cff6532a7ac4c7053e1dc8370b887facb29
gal pahima <galpahima2508@gmail.com>
2021-05-23 19:06:53 +0300

    style: move createRequestInfo to helper

--- Trivia/Trivia/Helper.cpp
+++ Trivia/Trivia/Helper.cpp
@@ -24,3 +24,14 @@ const std::string Helper::extractDetailsFromLine(std::string& line)
 	int found_at = line.find('=');
 	return line.substr(line.find('=')+1, line.length()-found_at);
 }
+
+RequestInfo Helper::createRequestInfo(std::vector<unsigned char> buffer)
+{
+	RequestInfo info = RequestInfo();
+
+	info.id = (int)buffer[0];
+
+	info.receivalTime = time(nullptr);
+
+	return RequestInfo();
+}
--- Trivia/Trivia/Helper.h
+++ Trivia/Trivia/Helper.h
@@ -3,10 +3,18 @@
 #include <tuple>
 #include <string>
 #include <fstream>
+#include "RequestInfo.h"
 
-class Helper {
+class Helper 
+{
 public:
 	//return ip, port
 	static std::pair<std::string, int> importDetailsFromConfig(std::string configName);
 	static const std::string extractDetailsFromLine(std::string& line);
+	
+	static RequestInfo createRequestInfo(std::vector<unsigned char> buffer);
+
+private: 
+	static int findDataLength();
+
 };
