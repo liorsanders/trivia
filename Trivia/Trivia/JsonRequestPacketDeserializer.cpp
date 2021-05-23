7357aeab4b8dd984ad09ce196aae80c478cb5c76
gal pahima <galpahima2508@gmail.com>
2021-05-23 18:37:38 +0300

    feat: add deserialize signup request option

--- Trivia/Trivia/JsonRequestPacketDeserializer.cpp
+++ Trivia/Trivia/JsonRequestPacketDeserializer.cpp
@@ -15,12 +15,36 @@ LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest
     try
     {
         request.username = data["username"];
-        request.username = data["password"];
+        request.password = data["password"];
     }
     catch (std::exception& e)
     {
         std::cerr << e.what();
     }
  
+    return request;
+}
+
+SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest
+    (std::vector<unsigned char> buffer)
+{
+    SignupRequest request = SignupRequest();
+
+    // json can accept string as a paramter so we convert the vector to string
+    nlohmann::json data = nlohmann::json::parse(
+        std::string(buffer.begin(), buffer.end()));
+
+    // in case some one try to trick us and there is no user name
+    try
+    {
+        request.username = data["username"];
+        request.password = data["password"];
+        request.email = data["email"];
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what();
+    }
+
     return request;
 }
