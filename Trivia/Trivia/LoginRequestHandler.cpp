4d1f0e86df309a890e2540138119a38afaf90702
gal pahima <galpahima2508@gmail.com>
2021-05-23 21:09:29 +0300

    feat: added handleRequest function

--- Trivia/Trivia/LoginRequestHandler.cpp
+++ Trivia/Trivia/LoginRequestHandler.cpp
@@ -1,8 +1,24 @@
 #include "LoginRequestHandler.h"
 #include "Codes.h"
+#include "LoginResponse.h"
+#include "JsonResponsePacketSerializer.h"
 
 bool LoginRequestHandler::isRequestRelevant(const RequestInfo& request)
 {
     return request.id == (int)Codes::Login;
+}
+
+RequestResult LoginRequestHandler::handleRequest(const RequestInfo& request)
+{
+    RequestResult result = RequestResult();
+    LoginResponse response = LoginResponse();
+
+    response.status = 1;
+
+    result.response = 
+        JsonResponsePacketSerializer::serializeResponse(response);
+    result.newHandler = nullptr;
+
+    return result;
 }
 
