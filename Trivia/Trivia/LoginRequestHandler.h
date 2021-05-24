d74761f502e12aa1f4383cb7a18412ac8e7f9b2d
lior sanders <sanderslior@gmail.com>
2021-05-24 17:26:34 +0300

    added more methods and fields to LoginRequestHandler

--- Trivia/Trivia/LoginRequestHandler.h
+++ Trivia/Trivia/LoginRequestHandler.h
@@ -1,13 +1,19 @@
 #pragma once
 
+#include "LoginManager.h"
 #include "LoginResponse.h"
 #include "JsonRequestPacketDeserializer.h"
 #include "RequestResult.h"
+#include "RequestHandlerFactory.h"
 #include "Codes.h"
 
 class LoginRequestHandler : public IRequestHandler
 {
-
+private:
+	RequestResult login(RequestInfo info);
+	RequestResult signup(RequestInfo info);
+	LoginManager& m_loginManager;
+	RequestHandlerFactory& m_handlerFactory;
 public:
 	bool isRequestRelevant(const RequestInfo& request) override;
 
