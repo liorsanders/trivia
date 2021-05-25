30991225ebcb19a99e221166fbe6c53caf5e34df
gal pahima <galpahima2508@gmail.com>
2021-05-25 21:18:03 +0300

    fix: added virtual dtor

--- Trivia/Trivia/IRequestHandler.h
+++ Trivia/Trivia/IRequestHandler.h
@@ -8,6 +8,7 @@ struct RequestResult;
 class IRequestHandler 
 {
 public:
+	virtual ~IRequestHandler() = default;
 	virtual bool isRequestRelevant(const RequestInfo& request) = 0;
 	virtual RequestResult handleRequest(const RequestInfo& request) = 0;
 };
