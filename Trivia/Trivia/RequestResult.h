3ec0188d5c3439d9a26c7b9f0ef4e1854c8cbfa9
gal pahima <galpahima2508@gmail.com>
2021-05-25 18:39:48 +0300

    pref: change dump pointer to shared pointer

--- Trivia/Trivia/RequestResult.h
+++ Trivia/Trivia/RequestResult.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include "IRequestHandler.h"
+#include <memory>
 
 class IRequestHandler;
 
 struct RequestResult
 {
 	std::vector<unsigned char> response; 
-	IRequestHandler* newHandler;
+	std::shared_ptr<IRequestHandler> newHandler;
 };
