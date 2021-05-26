1fd8227931678bd01ecd70f20f02e6ecb1ac7dac
lior sanders <sanderslior@gmail.com>
2021-05-26 15:54:37 +0300

    feat: added support for exceptions in login and signup situations

--- Trivia/Trivia/LoginManager.cpp
+++ Trivia/Trivia/LoginManager.cpp
@@ -1,4 +1,5 @@
 #include "LoginManager.h"
+#include "InvalidLoginException.h"
 #include <algorithm>
 #include <iostream>
 
@@ -30,14 +31,14 @@ void LoginManager::login(std::string username,
 	{
 		std::cerr << "Error: " << username << " does not exist." << std::endl;
 		
-		return;
+		throw InvalidLoginException("user does not exist");
 	}
 
 	if (!m_database->doesPasswordMatch(username, password))
 	{
 		std::cerr << "Error: " << username << " and " << password <<
 			" does not match." << std::endl;
-		return;
+		throw InvalidLoginException("invalid username or password");
 	}
 
 	m_loggedUsers.emplace_back(username);
@@ -49,7 +50,7 @@ void LoginManager::signup(const std::string username,
 	if (m_database->doesUserExist(username))
 	{
 		std::cerr << "Error: " << username << "is already exist." << std::endl;
-		return;
+		throw InvalidLoginException("user already exists");
 	}
 
 	m_database->addNewUser(username, password, mail);
