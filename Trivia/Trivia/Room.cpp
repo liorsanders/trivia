34771d7fe9f13a75df0a547429e845f2b1cf945e
gal pahima <galpahima2508@gmail.com>
2021-05-27 20:41:29 +0300

    feature: create getAllUsers, the function convert the vector of logged user to a vector of string.

--- Trivia/Trivia/Room.cpp
+++ Trivia/Trivia/Room.cpp
@@ -1,5 +1,7 @@
 #include "Room.h"
 
+#include <algorithm>
+
 void Room::addUser(LoggedUser user)
 {
 	m_users.push_back(user);
@@ -10,4 +12,21 @@ void Room::removeUser(LoggedUser user)
 	auto userIt = std::remove(m_users.begin(), m_users.end(), user);
 
 	m_users.erase(userIt);
+}
+
+std::vector<std::string> Room::getAllUsers()
+{
+	std::vector<std::string> allUsers;
+
+	auto getUsername = [](const LoggedUser& user) 
+							{ return user.getUsername(); };
+
+	std::transform(
+		m_users.begin(),
+		m_users.end(),
+		allUsers.begin(),
+		getUsername);
+
+
+	return allUsers;
 }
