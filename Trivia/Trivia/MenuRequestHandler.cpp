79ce015cdcaa87b711289586d77cccf174e430fe
gal pahima <galpahima2508@gmail.com>
2021-05-27 10:05:30 +0300

    style: function implementaion should be in cpp file!

--- Trivia/Trivia/MenuRequestHandler.cpp
+++ Trivia/Trivia/MenuRequestHandler.cpp
@@ -1,1 +1,11 @@
 #include "MenuRequestHandler.h"
+
+bool MenuRequestHandler::isRequestRelevant(const RequestInfo& request) const
+{
+    return false;
+}
+
+RequestResult MenuRequestHandler::handleRequest(const RequestInfo& request)
+{
+    return RequestResult();
+}
