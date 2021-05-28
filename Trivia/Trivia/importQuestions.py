c1d3f1416bc24baf251bdf2ce9dd4b96726ce3a3
gal pahima <galpahima2508@gmail.com>
2021-05-28 16:26:23 +0300

    feature: added create_data_dict function that convert the request into a python dict

--- Trivia/Trivia/importQuestions.py
+++ Trivia/Trivia/importQuestions.py
@@ -1,9 +1,36 @@
 import requests
 import sqlite3
+import random
 
+db_url = "https://opentdb.com/api.php?amount=10&type=multiple"
+http_success = 200
+
+
+def create_data_dict(data):
+    # extract parameters from data
+    question = data["question"]
+    correct_answer = data["correct_answer"]
+    incorrect_answers = data["incorrect_answers"]
+
+    answers = [correct_answer,
+               incorrect_answers[0],
+               incorrect_answers[1],
+               incorrect_answers[2]]
+
+    random.shuffle(answers)
+
+    return {"question": question,
+            "answers": answers,
+            "correct_answer": correct_answer}
 
 
 def main():
+    data = requests.get(db_url)
+
+    if data.status_code == http_success:
+        data = data.json()
+        dict = create_data_dict(data["results"][0])
+        print(dict)
 
 
 if __name__ == "__main__":
