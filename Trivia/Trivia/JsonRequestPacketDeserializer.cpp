cfcd4622e76064b6caff60e09cba8f08ede5995c
gal pahima <galpahima2508@gmail.com>
2021-05-30 19:28:26 +0300

    fix: remove test printing

--- Trivia/Trivia/JsonRequestPacketDeserializer.cpp
+++ Trivia/Trivia/JsonRequestPacketDeserializer.cpp
@@ -50,7 +50,7 @@ SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest
 }
 
 RequestInfo JsonRequestPacketDeserializer::
-createRequestInfo(const std::vector<unsigned char>& message)
+    createRequestInfo(const std::vector<unsigned char>& message)
 {
     RequestInfo info = RequestInfo();
 
