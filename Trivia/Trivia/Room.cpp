8c12a79d925752a85327074ce418b7fc26f497f6
gal pahima <galpahima2508@gmail.com>
2021-05-30 19:52:36 +0300

    pref: added mutex to improve multi threads

--- Trivia/Trivia/Room.cpp
+++ Trivia/Trivia/Room.cpp
@@ -2,6 +2,9 @@
 
 #include <algorithm>
 #include <iterator>
+#include <mutex>
+
+std::mutex usersLock;
 
 Room::Room(RoomData metadata) :
 	m_metadata(metadata)
@@ -10,11 +13,15 @@ Room::Room(RoomData metadata) :
 
 void Room::addUser(LoggedUser user)
 {
+	std::lock_guard<std::mutex> lock(usersLock);
+	
 	m_users.push_back(user);
 }
 
 void Room::removeUser(LoggedUser user)
 {
+	std::lock_guard<std::mutex> lock(usersLock);
+
 	auto userIt = std::remove(m_users.begin(), m_users.end(), user);
 
 	m_users.erase(userIt, m_users.end());
@@ -27,12 +34,15 @@ std::vector<std::string> Room::getAllUsers() const
 	auto getUsername = [](const LoggedUser& user) 
 							{ return user.getUsername(); };
 
+	std::unique_lock<std::mutex> lock(usersLock);
+
 	std::transform(
 		m_users.begin(),
 		m_users.end(),
 		std::back_inserter(allUsers),
 		getUsername);
 
+	lock.unlock();
 
 	return allUsers;
 }
