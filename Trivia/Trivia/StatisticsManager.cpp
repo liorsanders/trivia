920d04065f4111cfc046cf1e6aa6c9e1f20d2927
liorsanders <sanderslior@gmail.com>
2021-06-06 17:15:00 +0300

    made getHighScore function more readable and functional

--- Trivia/Trivia/StatisticsManager.cpp
+++ Trivia/Trivia/StatisticsManager.cpp
@@ -14,19 +14,18 @@ std::string getName(const std::map<std::string, float>& Map,
 
 std::vector<std::string> StatisticsManager::getHighScore() const
 {
-	std::array<float, 5> topScores = { -999, -999, -999, -999, -999 };
-	std::array<std::string, 5> topUsers;
-	const auto scores = m_database->getUsersScores();
-	for (auto it = scores.begin(); it != scores.end(); ++it) {
-		if (topScores[4] < it->second) {
-			topScores[4] = it->second;
-		}
-		std::sort(topScores.begin(), topScores.end(), std::greater<>());
-	}
-	for (int i = 0; i < 5; i++) {
-		topUsers[i] = getName(scores, topScores[i]);
-	}
-	return std::vector<std::string>(topUsers.begin(), topUsers.end());
+	std::vector<std::string> res;
+	auto scores = m_database->getUsersScores();
+	auto scoresVector = { scores.begin(), scores.end() };
+	std::partial_sort(scoresVector.begin(), scoresVector.end(), scoresVector.begin() + 5,
+		[](const auto& lhs, const auto& rhs) {
+			return lhs.second > rhs.second;
+		});
+	std::for_each(scoresVector.begin(), scoresVector.begin() + 5, [&](std::pair<const std::string, const float>& element) {
+		res.push_back(element.first);
+		});
+	std::reverse(res.begin(), res.end());
+	return res;
 }
 
 std::vector<std::string> StatisticsManager::getUserStatistics(const std::string& username) const
