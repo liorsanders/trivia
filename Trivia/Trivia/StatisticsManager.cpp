c3f5e811f8fedb04f59d7453cbf6b16cbf51cf37
liorsanders <sanderslior@gmail.com>
2021-06-06 16:00:05 +0300

    finished statistics manager implementation

--- Trivia/Trivia/StatisticsManager.cpp
+++ Trivia/Trivia/StatisticsManager.cpp
@@ -33,7 +33,11 @@ std::vector<std::string> StatisticsManager::getHighScore() const
 	return std::vector<std::string>(topUsers.begin(), topUsers.end());
 }
 
-std::vector<std::string> StatisticsManager::getUserStatistics(const std::string& usernmae) const
+std::vector<std::string> StatisticsManager::getUserStatistics(const std::string& username) const
 {
-	return std::vector<std::string>();
+	std::string numOfGames = std::to_string(m_database->getNumOfPlayerGames(username));
+	std::string averageAnswerTime = std::to_string(m_database->getPlayersAverageAnswerTime(username));
+	std::string numOfCorrect = std::to_string(m_database->getNumOfCorrectAnswers(username));
+	std::string totalNumOfAnswers = std::to_string(m_database->getNumOfTotalAnswers(username));
+	return { numOfCorrect, numOfGames, numOfCorrect, totalNumOfAnswers };
 }
--- Trivia/Trivia/StatisticsManager.h
+++ Trivia/Trivia/StatisticsManager.h
@@ -8,7 +8,7 @@ class StatisticsManager
 {
 public:
 	std::vector<std::string> getHighScore() const;
-	std::vector<std::string> getUserStatistics(const std::string& usernmae) const;
+	std::vector<std::string> getUserStatistics(const std::string& username) const;
 private:
 	IDatabase* m_database;
 };
