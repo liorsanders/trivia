f9f1f3e6708721e26cae9aa3cda53d5008415738
liorsanders <sanderslior@gmail.com>
2021-06-06 18:27:44 +0300

    added MenuRequestHandler and RequestHandlerFactory new methods

--- Trivia/Trivia/MenuRequestHandler.cpp
+++ Trivia/Trivia/MenuRequestHandler.cpp
@@ -1,5 +1,9 @@
 #include "MenuRequestHandler.h"
 
+MenuRequestHandler::MenuRequestHandler(const std::string username, RoomManager& roomManager,
+    StatisticsManager& statisticsManager, RequestHandlerFactory& factory) : m_user(LoggedUser(username)),
+    m_statisticsManager(statisticsManager), m_handlerFactory(factory), m_roomManager(roomManager) {}
+
 bool MenuRequestHandler::isRequestRelevant(const RequestInfo& request) const
 {
     return false;
--- Trivia/Trivia/RequestHandlerFactory.cpp
+++ Trivia/Trivia/RequestHandlerFactory.cpp
@@ -22,7 +22,22 @@ LoginRequestHandler* RequestHandlerFactory::createLoginRequestHandler()
     return res;
 }
 
+MenuRequestHandler* RequestHandlerFactory::createMenuRequestHandler()
+{
+    MenuRequestHandler* newMenuRequestHandler = new MenuRequestHandler("", m_roomManager, m_statisticsManager, *this);;
+}
+
+RoomManager& RequestHandlerFactory::getRoomManager()
+{
+    return m_roomManager;
+}
+
 LoginManager& RequestHandlerFactory::getLoginManager()
 {
     return m_loginManager;
+}
+
+StatisticsManager& RequestHandlerFactory::getStatisticsManager()
+{
+    return m_statisticsManager;
 }
