67ba0d11e66ea44e1b843ce1a9c5a70df2369dfa
gal pahima <galpahima2508@gmail.com>
2021-06-13 14:50:51 +0300

    feat: add email button

--- Client/Client/Client/Signup.xaml.cs
+++ Client/Client/Client/Signup.xaml.cs
@@ -24,5 +24,144 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        public void TB_Username_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+
+            if (tb.Text == "username or email")
+            {
+                tb.Text = string.Empty;
+            }
+            else
+            {
+                tb.GotFocus -= TB_Username_GotFocus;
+            }
+        }
+
+        private void TB_Username_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+
+            if (tb.Text == string.Empty)
+            {
+                tb.Text = "username or email";
+            }
+            else
+            {
+                tb.LostFocus -= TB_Username_LostFocus;
+            }
+        }
+
+        private void TB_Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+
+            if (tb.Text == "password")
+            {
+                tb.Text = string.Empty;
+            }
+            else
+            {
+                tb.GotFocus -= TB_Username_GotFocus;
+            }
+        }
+
+        private void TB_Password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+
+            if (tb.Text == string.Empty)
+            {
+                tb.Text = "password";
+            }
+            else
+            {
+                tb.LostFocus -= TB_Username_LostFocus;
+            }
+        }
+
+        private void TB_Password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+
+            if (tb.Text != "password" && tb.Text != "")
+            {
+                Color color = (Color)ColorConverter.ConvertFromString("#2a2e47");
+
+                tb.Foreground = new SolidColorBrush(color);
+
+                tb.FontFamily = new FontFamily("Password");
+            }
+        }
+
+        private void TB_Username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+
+            if (tb.Text != "username or email" && tb.Text != "")
+            {
+                Color color = (Color)ColorConverter.ConvertFromString("#2a2e47");
+
+                tb.Foreground = new SolidColorBrush(color);
+            }
+        }
+
+        private void CB_ShowPassword_Checked(object sender, RoutedEventArgs e)
+        {
+
+            TB_Password.FontFamily = new FontFamily("Courier");
+        }
+
+        private void CB_ShowPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (TB_Password.Text != "password" && TB_Password.Text != "")
+            {
+                TB_Password.FontFamily = new FontFamily("Password");
+            }
+        }
+
+        private void Bt_Signup_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button bt = sender as Button;
+
+            Color color = (Color)ColorConverter.ConvertFromString("#6711f2");
+
+            bt.Foreground = new SolidColorBrush(color);
+        }
+
+        private void Bt_Signup_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+            Button bt = sender as Button;
+
+            Color color = (Color)ColorConverter.ConvertFromString("#8f50f6");
+
+            bt.Foreground = new SolidColorBrush(color);
+        }
+
+        private void Bt_Signup_Click(object sender, RoutedEventArgs e)
+        {
+            Button bt = sender as Button;
+
+            Color color = (Color)ColorConverter.ConvertFromString("#33067a");
+
+            bt.Foreground = new SolidColorBrush(color);
+        }
+
+        private void TB_Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TB_Email_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TB_Email_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
