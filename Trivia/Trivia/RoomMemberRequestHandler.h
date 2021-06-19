1fdee5f11053ba87c1291b8f226bd2488ea4edbc
gal pahima <galpahima2508@gmail.com>
2021-06-19 17:17:47 +0300

    chore: add empty function declartion and fields

--- Trivia/Trivia/RoomMemberRequestHandler.h
+++ Trivia/Trivia/RoomMemberRequestHandler.h
@@ -1,7 +1,22 @@
 #pragma once
 #include "IRequestHandler.h"
+#include "Room.h"
+#include "RoomManager.h"
+#include "LoggedUser.h"
+#include "RequestHandlerFactory.h"
 
 class RoomMemberRequestHandler : public IRequestHandler
 {
+	bool isRequestRelevant(RequestInfo);
+	RequestResult handleRequest(RequestInfo);
+
+private:
+	Room m_room;
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	RequestHandlerFactory& m_handlerFactory;
+
+	RequestResult leaveRoom(RequestInfo);
+	RequestResult getRoomState(RequestInfo);
 };
 
