33f322ad04467a868e1f4efee1c8739335bb1c57
gal pahima <galpahima2508@gmail.com>
2021-06-22 00:06:57 +0300

    build: create room admin request handler

---
+++ Trivia/Trivia/RoomAdminRequestHandler.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "IRequestHandler.h"
+#include "Room.h"
+#include "RoomManager.h"
+#include "LoggedUser.h"
+#include "RequestHandlerFactory.h"
+#include "Codes.h"
+
+class RoomAdminRequestHandler :
+    public IRequestHandler
+{
+public: 
+	bool isRequestRelevant(RequestInfo);
+	RequestResult handleRequest(RequestInfo);
+
+
+private: 
+	Room m_room;
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	RequestHandlerFactory& m_handlerFactory;
+
+	RequestResult closeRoom(RequestInfo);
+	RequestResult startGame(RequestInfo);
+	RequestResult getRoomState(RequestInfo);
+};
+
