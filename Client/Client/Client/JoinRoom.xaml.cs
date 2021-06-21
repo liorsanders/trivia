c0b415dfba29f78d685a81b4abcb24924a0ad264
liorsanders <sanderslior@gmail.com>
2021-06-21 16:49:34 +0300

    added Thread to recieve rooms from server

--- Client/Client/Client/JoinRoom.xaml.cs
+++ Client/Client/Client/JoinRoom.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Client
 {
@@ -22,28 +23,34 @@ namespace Client
     {
         private readonly Frame _main;
         private readonly string _username;
+        private Thread roomUpdaterThread;
         public JoinRoom(Frame main, string username)
         {
             InitializeComponent();
             _main = main;
             _username = username;
             usernameBlock.Text = username;
-            updateRooms();
+            roomUpdaterThread = new Thread(new ThreadStart(updateRooms));
+            roomUpdaterThread.Start();
         }
         private void updateRooms()
         {
             //this function accesses roomsGrid and adds buttons of rooms by the number of rooms
+
+            Thread.Sleep(3);
         }
         private void BT_Exit_Click(object sender, RoutedEventArgs e)
         {
             if (Application.Current.MainWindow is MainWindow main)
             {
+                roomUpdaterThread.Suspend();
                 main.Close();
             }
         }
 
         private void BT_Menu_Click(object sender, RoutedEventArgs e)
         {
+            roomUpdaterThread.Suspend();
             _main.Content = new MainMenu(_main, _username);
         }
     }
