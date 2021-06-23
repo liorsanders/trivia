b8f6e4ceab44b6c885f0b88b3d64373c9f4ec41f
liorsanders <sanderslior@gmail.com>
2021-06-23 13:28:59 +0300

    added Codes enum

--- Client/Client/Client/JsonRequestPacketDeserializer.cs
+++ Client/Client/Client/JsonRequestPacketDeserializer.cs
@@ -8,5 +8,6 @@ namespace Client
 {
     class JsonRequestPacketDeserializer
     {
+        
     }
 }
--- Client/Client/Client/JsonRequestPacketSerializer.cs
+++ Client/Client/Client/JsonRequestPacketSerializer.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+enum Codes
+{
+	Signup = 73,
+	Login = 25,
+	Error = 8,
+	Logout = 98,
+	CreateRoom = 88,
+	JoinRoom = 44, 
+	GetRoom = 15,
+	GetPlayers = 64,
+	Statistics = 30,
+	CloseRoom = 67,
+	Start = 4,
+	GetRoomState = 16,
+	LeaveRoom = 49
+};
 namespace Client
 {
-    class JsonRequestPacketSerializer
+    static class JsonRequestPacketSerializer
     {
+        static void serializeLoginRequest(string username, string password)
+        {
+
+        }
     }
 }
